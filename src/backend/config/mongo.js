2c15a1bd667569593f608e422070c0a0cbc94ae9
frank yanez <frankyanez9413@gmail.com>
2023-07-24 17:14:01 -0300

    corrijo seguridad

--- src/backend/config/mongo.js
+++ src/backend/config/mongo.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 
 export const dbConnect = ()=>{
-    const DB_URI = 'mongodb+srv://frankyanez9413:FfNYEsZVj8UlP3nh@u-play.q17ha7v.mongodb.net/songs?retryWrites=true&w=majority';
+    const DB_URI = process.env.MONGO_URI;
     mongoose.connect(DB_URI)
     .then(()=> console.log('connected'))
     .catch(err => console.log(err))
